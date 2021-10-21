d8842205379ec30152de6a124a9feb76609856c9
Daniel Thompson <kritterdoggo@gmail.com>
2021-10-21 17:13:35 -0400

    Added SpaceDAO & Space

---
+++ Capstone/Capstone/Models/Space.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class Space
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string OpeningMonth { get; set; }
+
+        public string ClosingMonth { get; set; }
+
+        public int MaxOccupancy { get; set; }
+
+        public bool WheelchairAccessible { get; set; }
+
+        public decimal DailyRate { get; set; }
+    }
+}
